469bdc3874447cba7db9f2a6d6ff5da65a4a7b54
Jack Wilburn <jackwilburn@tutanota.com>
2022-11-11 14:50:23 -0700

    Make table reactive to store changes

--- src/components/OverviewTable.vue
+++ src/components/OverviewTable.vue
@@ -39,11 +39,12 @@ import { axisBottom } from 'd3-axis';
 import { scaleLinear, ScaleLinear } from 'd3-scale';
 import TableCell from './TableCell.vue';
 import TableAxis from './TableAxis.vue';
+import { computed } from 'vue';
 
 // Load data
 const store = useDataStore();
-const tableRows = store.tableData.tableRows;
-const tableTotals = store.tableData.tableTotals;
+const tableRows = computed(() => store.tableData.tableRows);
+const tableTotals = computed(() => store.tableData.tableTotals);
 const sentimentTypes: SentimentOptions[] = [
       'Tweet text support',
       'Tweet text oppose',
